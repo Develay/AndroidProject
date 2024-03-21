82bd9673d6676d2e3638abd7579148abac9501cb
baptou2110 <baptistedupe2110@gmail.com>
2024-03-21 11:01:18 +0100

    Link main to memory

--- app/src/main/java/com/example/android_project/MainActivity.java
+++ app/src/main/java/com/example/android_project/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.android_project.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ActivityMainBinding binding;
 
@@ -15,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        binding.play.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, Memory.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/android_project/Memory.java
+++ app/src/main/java/com/example/android_project/Memory.java
@@ -2,23 +2,40 @@ package com.example.android_project;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.example.android_project.databinding.ActivityMainBinding;
 import com.example.android_project.databinding.ActivityMemoryBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Memory extends AppCompatActivity {
 
     private ActivityMemoryBinding binding;
+    private List<Card> fragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMemoryBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        fragments = new ArrayList<>();
+        fragments.add(Card.newInstance("2+6=", "8"));
+        fragments.add(Card.newInstance("6+0=", "6"));
+        fragments.add(Card.newInstance("5+6=", "11"));
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        for (Card frag : fragments) {
+            ft.add(R.id.fragment_container1, frag);
+        }
+        fragments = new ArrayList<>();
+        fragments.add(Card.newInstance("2+6=", "8"));
+        fragments.add(Card.newInstance("6+0=", "6"));
+        fragments.add(Card.newInstance("5+6=", "11"));
+
+        for (Card frag : fragments) {
+            ft.add(R.id.fragment_container2, frag);
+        }
+        ft.commit();
     }
 }
