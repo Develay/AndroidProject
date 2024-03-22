5ec220d37ae766004e598e42001b2a12f16c1a11
baptou2110 <baptistedupe2110@gmail.com>
2024-03-22 21:32:32 +0100

    Memory adaptation

--- app/src/main/java/com/example/android_project/Memory.java
+++ app/src/main/java/com/example/android_project/Memory.java
@@ -1,84 +1,87 @@
 package com.example.android_project;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.GridView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.example.android_project.databinding.ActivityMemoryBinding;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+
+
 
 public class Memory extends AppCompatActivity {
 
     private ActivityMemoryBinding binding;
-    private List<Card> fragments;
+    private GridView gridView;
+    private MemoryAdapter adapter;
+    private ArrayList<Integer> cards;
+    private int firstCard, secondCard;
+    private boolean isFirst = true;
+
+    public Memory() {
+        cards = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            cards.add(i);
+        }
+        adapter = new MemoryAdapter(this, cards);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMemoryBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        fragments = new ArrayList<>();
-        fragments.add(Card.newInstance("2+6=", "8"));
-        fragments.add(Card.newInstance("6+0=", "6"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        for (Card frag : fragments) {
-            ft.add(R.id.fragment_container1, frag);
-        }
-        fragments = new ArrayList<>();
-        fragments.add(Card.newInstance("2+6=", "8"));
-        fragments.add(Card.newInstance("6+0=", "6"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        fragments.add(Card.newInstance("5+6=", "11"));
+        setContentView(R.layout.activity_memory);
 
-        for (Card frag : fragments) {
-            ft.add(R.id.fragment_container2, frag);
-        }
-        fragments = new ArrayList<>();
-        fragments.add(Card.newInstance("2+6=", "8"));
-        fragments.add(Card.newInstance("6+0=", "6"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        fragments.add(Card.newInstance("5+6=", "11"));
+        gridView = findViewById(R.id.gridView);
 
-        for (Card frag : fragments) {
-            ft.add(R.id.fragment_container3, frag);
+        if (gridView != null) {
+            gridView.setAdapter(adapter);
+        } else {
+            Log.e("Memory", "GridView is null");
         }
-        fragments = new ArrayList<>();
-        fragments.add(Card.newInstance("2+6=", "8"));
-        fragments.add(Card.newInstance("6+0=", "6"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        fragments.add(Card.newInstance("5+6=", "11"));
 
-        for (Card frag : fragments) {
-            ft.add(R.id.fragment_container4, frag);
-        }
-        fragments = new ArrayList<>();
-        fragments.add(Card.newInstance("2+6=", "8"));
-        fragments.add(Card.newInstance("6+0=", "6"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        for (Card frag : fragments) {
-            ft.add(R.id.fragment_container5, frag);
-        }
-        fragments = new ArrayList<>();
-        fragments.add(Card.newInstance("2+6=", "8"));
-        fragments.add(Card.newInstance("6+0=", "6"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        fragments.add(Card.newInstance("5+6=", "11"));
-        fragments.add(Card.newInstance("5+6=", "11"));
+        //Click listener for grid view
+        gridView.setOnItemClickListener((parent, view, position, id) -> {
+            if (isFirst) {
+                firstCard = position;
+                adapter.showCard(firstCard);
+                isFirst = false;
+            } else {
+                secondCard = position;
+                checkMatch();
+                isFirst = true;
+            }
+        });
 
-        for (Card frag : fragments) {
-            ft.add(R.id.fragment_container6, frag);
+        //Click listener for reset button
+        findViewById(R.id.resetButton).setOnClickListener(v -> resetGame());
+    }
+
+    private void checkMatch() {
+        if ((cards.get(firstCard).equals(cards.get(secondCard)))) {
+            // Matched
+
+        } else {
+            // Not matched, hide the cards
+            gridView.postDelayed(() -> {
+                adapter.hideCard(firstCard);
+                adapter.hideCard(secondCard);
+            }, 1000); // Delay to allow the user to see the cards
         }
-        ft.commit();
+    }
+
+    private void resetGame() {
+        Collections.shuffle(cards);
+        adapter = new MemoryAdapter(this, cards);
+        gridView.setAdapter(adapter);
     }
 }
+
+
+
+
+
+
